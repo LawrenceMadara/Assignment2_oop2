2be5f8caa89f20a16921251e9fb09b5dce167cbb
LawrenceMadara <mark.madara@edu.sait.ca>
2024-11-17 23:38:59 -0700

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Assignment2BlazorApp.Data;
+
+namespace Assignment2BlazorApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Assignment2BlazorApp.Data;
+
+namespace Assignment2BlazorApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
